2f7376d31f17af830eecd23c38d14c3797cecb53
Delfina Sarkis <delfysark17@gmail.com>
2026-09-21 15:37:02 -0300

    Completo ABML de DaoProducto - modificar y listar productos

--- src/dao/DaoProducto.java
+++ src/dao/DaoProducto.java
@@ -3,6 +3,8 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import entidad.Producto;
 
@@ -54,4 +56,53 @@ public class DaoProducto {
 			return 0;
 		}
 	}
+	
+	public int modificarProducto(Producto producto) {
+		String query = "UPDATE Productos SET Nombre = ?, Precio = ?, Stock = ?, IdCategoria = ? WHERE Codigo = ?";
+		
+		try (Connection cn = DriverManager.getConnection(host + dbName, user, pass);
+			 PreparedStatement st = cn.prepareStatement(query))
+		{
+			st.setString(1, producto.getNombre());
+			st.setDouble(2, producto.getPrecio());
+			st.setInt(3, producto.getStock());
+			st.setInt(4, producto.getIdCategoria());
+			st.setString(5, producto.getCodigo());
+			
+			return st.executeUpdate();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	
+public ArrayList<Producto> obtenerTodosLosProductos(){
+	ArrayList<Producto> lista = new ArrayList<Producto>();
+	String query = "SELECT * FROM Productos";
+	
+	try(Connection cn = DriverManager.getConnection(host + dbName, user, pass);
+			PreparedStatement st = cn.prepareStatement(query);
+			ResultSet rs = st.executeQuery())
+	{
+		while (rs.next())
+		{
+			Producto p = new Producto();
+			p.setCodigo(rs.getString("Codigo"));
+			p.setNombre(rs.getString("Nombre"));
+			p.setPrecio(rs.getDouble("Precio"));
+			p.setStock(rs.getInt("Stock"));
+			p.setIdCategoria(rs.getInt("IdCategoria"));
+			lista.add(p);
+		}
+	}
+	catch (Exception e)
+	{
+		e.printStackTrace();
+	}
+	
+	return lista;
+}
 }
