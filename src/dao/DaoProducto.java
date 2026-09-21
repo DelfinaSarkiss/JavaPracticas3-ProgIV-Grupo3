d30043d2ac0ab241f9c55ad362e942ce6f93f52e
Miwa Martinez <miwa.mtz@gmail.com>
2026-09-21 19:28:03 -0300

    Agrego metodo agregarUnProducto()- Punto D

--- src/dao/DaoProducto.java
+++ src/dao/DaoProducto.java
@@ -104,5 +104,25 @@ public ArrayList<Producto> obtenerTodosLosProductos(){
 	}
 	
 	return lista;
+	
+	
 }
+
+	public void agregarUnProducto() {
+		Producto producto1 = new Producto("prod1", "Shampoo", 1500.99, 18, 1);
+
+		int filas = this.agregarProducto(producto1);
+
+		if(filas ==1)
+			System.out.println("Producto agregado");
+		else
+			System.out.println("Producto no agregado");
+
+
+}
+	public static void main(String[] args) {
+	    DaoProducto dao = new DaoProducto();
+	    dao.agregarUnProducto();
+	}
+	
 }
